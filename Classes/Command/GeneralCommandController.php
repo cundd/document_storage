afb98b2ba6125a038f8ff114709ef3b137d21604
Daniel Corn <cod@iresults.li>
2019-05-28 18:04:11 +0200

    Allow to pass a GUID as argument to the general command

--- Classes/Command/GeneralCommandController.php
+++ Classes/Command/GeneralCommandController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use function explode;
+use function strpos;
 
 class GeneralCommandController extends AbstractCommandController
 {
@@ -18,16 +20,44 @@ class GeneralCommandController extends AbstractCommandController
         $help = 'Read Documents from a database or list all databases.';
         $this->setDescription('Show Documents and databases')
             ->setHelp($help)
-            ->addArgument('database', InputArgument::OPTIONAL, 'Name of the database (omit to list all databases)')
-            ->addArgument('id', InputArgument::OPTIONAL, 'ID of the Document to show (omit to show all)')
-            ->addOption('count', 'c', InputOption::VALUE_NONE, 'Count Documents instead of displaying (only w/ [<id>])')
-            ->addOption('short', 's', InputOption::VALUE_NONE, 'Show only the headers of Documents (only w/ [<id>])');
+            ->addArgument(
+                'database',
+                InputArgument::OPTIONAL,
+                'Name of the database or GUID (omit to list all databases)'
+            )
+            ->addArgument(
+                'id',
+                InputArgument::OPTIONAL,
+                'ID of the Document to show (omit to show all)'
+            )
+            ->addOption(
+                'count',
+                'c',
+                InputOption::VALUE_NONE,
+                'Count Documents instead of displaying (only w/ [<id>])'
+            )
+            ->addOption(
+                'short',
+                's',
+                InputOption::VALUE_NONE,
+                'Show only the headers of Documents (only w/ [<id>])'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $database = $input->getArgument('database');
         $id = $input->getArgument('id');
+
+        if (strpos($database, '/') !== false) {
+            if ($id) {
+                $output->writeln('<error>If a GUID is given, the id argument must be omitted</error>');
+
+                return 1;
+            }
+
+            list($database, $id) = explode('/', $database);
+        }
 
         if ($database) {
             $command = $this->getApplication()->find('document-storage:read');
